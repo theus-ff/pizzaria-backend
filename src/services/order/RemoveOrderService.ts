f80be737ca29d5218990bc84575f059e36490297
theus-ff <matheusfelixflores@gmail.com>
2024-11-19 20:20:56 -0300

    Deploy Projeto Pizzaria API

---
+++ src/services/order/RemoveOrderService.ts
@@ -0,0 +1,25 @@
+import prismaClient from "../../prisma";
+
+interface OrderRequest{
+  order_id: string;
+}
+
+class RemoveOrderService{
+  async execute({ order_id }: OrderRequest){
+    await prismaClient.item.deleteMany({
+      where:{
+        order_id: order_id
+      }
+    })
+    
+    const order = await prismaClient.order.delete({
+      where:{
+        id: order_id
+      }
+    })
+
+    return order
+  }
+}
+
+export { RemoveOrderService }
